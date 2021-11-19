b064a25555c0f28002a8fc02544f961bf6aeb11f
Cherie <c.kong@mail.utoronto.ca>
2021-11-18 23:06:19 -0500

    modified social media icons

--- a-tier-bonding/src/components/footer/footer.js
+++ a-tier-bonding/src/components/footer/footer.js
@@ -1,15 +1,13 @@
 import { Button } from '@mui/material';
 import { Box } from '@mui/system';
 import React from 'react';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import YouTubeIcon from '@mui/icons-material/YouTube';
+import { SocialIcon } from 'react-social-icons';
 import "./footer.css"
 const FootBarStyles = ({
   display: 'flex', 
   justifyContent: 'flex-end',  
   backgroundColor: 'rgb(249, 249, 249)',
-  padding: '0vh 20vh 1vh 0vh ',
+  padding: '0vh 10vh 1vh 0vh ',
   borderTop: "3px solid rgb(151, 182, 191)",
 });
 function Footer() {
@@ -17,9 +15,12 @@ function Footer() {
     <div>
       <Box sx={FootBarStyles}>
         <h4> Follow us on social media ! </h4> 
-        <Button href='https://twitter.com/Lets_A_Game'> <TwitterIcon/> </Button>
-        <Button> <InstagramIcon/> </Button>
-        <Button> <YouTubeIcon/> </Button>
+        <Button><SocialIcon url="https://www.youtube.com/channel/UCJHP_c33l2zt8KnRnD3mHrA" style={{ height: 35, width: 35 }}/> </Button>
+        <Button><SocialIcon url="https://www.facebook.com/lets.koilins.3" style={{ height: 35, width: 35 }}/> </Button>
+        <Button><SocialIcon url="https://twitter.com/Lets_A_Game" style={{ height: 35, width: 35 }}/> </Button>
+        <Button><SocialIcon url="https://www.instagram.com/lets_a_game/?hl=en" style={{ height: 35, width: 35 }}/> </Button>
+        <Button><SocialIcon url="https://www.twitch.tv/phieki" style={{ height: 35, width: 35 }}/> </Button>
+        <Button><SocialIcon url="https://discord.gg/8sHMkkg2" style={{ height: 35, width: 35 }}/> </Button>
       </Box>
     </div>
   );
