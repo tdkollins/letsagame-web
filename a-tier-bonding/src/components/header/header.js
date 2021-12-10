a15cf5dbdb28be516f7340433af8f46f5eaeb4a1
tdkollins <46738793+tdkollins@users.noreply.github.com>
2021-12-09 21:56:15 -0500

    Revert "Add Mobile Formatting"

--- a-tier-bonding/src/components/header/header.js
+++ a-tier-bonding/src/components/header/header.js
@@ -27,8 +27,7 @@ const NavBarStylesNotFixed = ({
   padding: '0vh 20vh 1vh 5vh ',
   borderBottom: "3px solid rgb(151, 182, 191)",
   alignItems: "center",
-  zIndex: "0",
-  minWidth: "1300px" 
+  zIndex: "0"
 });
 
 
@@ -44,7 +43,12 @@ function Header() {
         </ButtonGroup> 
       </Box>
       <Box sx={NavBarStylesNotFixed}>
-        <div style={{ height: "75px"}}/>
+        <img src={earthLogo} style={{ height: "75px", width: "75px" }} alt="Logo"/>
+        <ButtonGroup color='inherit' size="large" variant="text">
+          <Button component={Link} to='/'  style={{ fontSize: '2vh', fontFamily: "Nevis" }}>  Home  </Button>
+          <Button component={Link} to='/schedule' style={{ fontSize: '2vh', fontFamily: "Nevis" }}> Schedule  </Button> 
+          {/* <Button component={Link} to='/schedule'> Team </Button>  */}
+        </ButtonGroup> 
       </Box>
     </div>
   );
