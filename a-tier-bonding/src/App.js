7dc31b2494a378557a5763ff523d34646cd665f2
Cherie <c.kong@mail.utoronto.ca>
2024-12-24 19:05:46 -0500

    Updated packages

--- a-tier-bonding/src/App.js
+++ a-tier-bonding/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from './routes/homepage/homepage';
 import Header from './components/header/header';
 import Schedule from './routes/schedule/schedule'; 
@@ -12,13 +12,15 @@ function App() {
     <div>  
     <Router>
       <Header />
-      <Route path="/" exact component={Home} />
-      <Route path="/schedule" exact component={Schedule} />
-      <Route path="/team" exact component={Team} />
-      <Route path="/sweepstakes" render={() => {
-        window.location.href = '/sweepstakes.pdf';
-        return null;
-      }}/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/schedule" element={<Schedule/>} />
+        <Route path="/team" element={<Team/>} />
+        <Route path="/sweepstakes" render={() => {
+          window.location.href = '/sweepstakes.pdf';
+          return null;
+        }}/>
+      </Routes>
       <Footer/> 
     </Router>
     </div>
--- a-tier-bonding/src/components/countdown/countdown.js
+++ a-tier-bonding/src/components/countdown/countdown.js
@@ -1,31 +1,31 @@
 import {React} from 'react';
 import Box from '@mui/material/Box';
 import './countdown.css'
-import Timer from 'react-compound-timer'
+import {createTimeModel, useTimeModel} from 'react-compound-timer'
 
 
 var t1 = new Date();
 var t2 = new Date("2025-01-01T08:00:00.000-05:00");
 var dif = t2.getTime() - t1.getTime();
 
+const timer = createTimeModel({
+  initialTime: dif,
+  direction: "backward",
+});
+
+
 function Countdown() {
+  const { value } = useTimeModel(timer);   
   return (
     <div>
       <Box className="timer-box">
         <h2> Let's-a-Game begins in </h2> 
-          <Timer
-              initialTime={dif}
-              direction="backward"
-          >
-            {() => (
-                <div className="timer-display">
-                    <Timer.Days formatValue={value => value >= 10 ? `${value}`: `0${value}`}/> :
-                    <Timer.Hours formatValue={value => value >= 10 ? ` ${value}`: ` 0${value}`}/> :
-                    <Timer.Minutes  formatValue={value => value >= 10 ? ` ${value}`: ` 0${value}`}/> :
-                    <Timer.Seconds formatValue={value => value >= 10 ? ` ${value}`: ` 0${value}`}/>
-                </div>
-            )}
-          </Timer>
+          <div className="timer-display">
+            <div className="timer-display-text"> {value.d >= 10 ? value.d : `0${value.d}`} </div>
+            <div className="timer-display-text"> {value.h >= 10 ? value.h : `0${value.h}`} </div>
+            <div className="timer-display-text"> {value.m >= 10 ? value.m : `0${value.m}`} </div>
+            <div className="timer-display-text"> {value.s >= 10 ? value.s : `0${value.s}`} </div>
+          </div>
           <div className="timer-text-box">
             <div className="timer-text-display">
               <h3> Days </h3>
